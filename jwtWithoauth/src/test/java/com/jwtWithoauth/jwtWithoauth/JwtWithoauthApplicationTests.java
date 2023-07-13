dc004b20857ef7f656dddb5b7dd6aaeb536a32fb
최영태 <dudxo7721@naver.com>
2023-07-14 02:34:00 +0900

    프로젝트 생성

---
+++ jwtWithoauth/src/test/java/com/jwtWithoauth/jwtWithoauth/JwtWithoauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwtWithoauth.jwtWithoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtWithoauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
