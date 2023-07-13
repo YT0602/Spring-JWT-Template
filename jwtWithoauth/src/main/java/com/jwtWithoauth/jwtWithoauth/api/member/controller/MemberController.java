fcfa9f6de46880fd13f747ac228f601da7c63e0e
최영태 <dudxo7721@naver.com>
2023-07-14 02:35:33 +0900

    feat: 자제 회원가입

---
+++ jwtWithoauth/src/main/java/com/jwtWithoauth/jwtWithoauth/api/member/controller/MemberController.java
@@ -0,0 +1,25 @@
+package com.jwtWithoauth.jwtWithoauth.api.member.controller;
+
+import com.jwtWithoauth.jwtWithoauth.domain.member.dto.MemberSignDto;
+import com.jwtWithoauth.jwtWithoauth.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/test/jwt")
+    public String test(){
+        return "jwt 요청 성공";
+    }
+    // 회원가입 api
+    @PostMapping("/signup")
+    public String signUp(@RequestBody MemberSignDto memberSignDto) throws Exception{
+        memberService.signUp(memberSignDto);
+        return "회원가입 완료";
+    }
+
+}
