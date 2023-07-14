10d3dcfeeb67a206a1dd61f362fae779815edd78
최영태 <dudxo7721@naver.com>
2023-07-15 00:29:02 +0900

    feat: 로그인 폼 커스텀

---
+++ jwtWithoauth/src/main/java/com/jwtWithoauth/jwtWithoauth/global/customLogin/handler/LoginSuccessHandler.java
@@ -0,0 +1,56 @@
+package com.jwtWithoauth.jwtWithoauth.global.customLogin.handler;
+
+import com.jwtWithoauth.jwtWithoauth.domain.member.repository.MemberRepository;
+import com.jwtWithoauth.jwtWithoauth.global.jwt.service.JwtService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@RequiredArgsConstructor
+// 로그인 성공
+public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtService jwtService;
+    private final MemberRepository memberRepository;
+
+    @Value("${jwt.access.expiration}")
+    private String accessTokenExpiration;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) {
+        String email = extractUsername(authentication);
+        // 토큰 발급
+        // 유저 회원가입 시에는 RefreshToken이 없기 때문에 RefreshToken Column이 null 이다
+        // 따라서, 로그인 성공 시 RefeshToken을 발급하고, DB에 저장
+        String accessToken = jwtService.createAccessToken(email);
+        String refreshToken = jwtService.createRefreshToken();
+
+        // 응답 헤더에 AccessToken, RefreshToken 실어서 응답
+        jwtService.sendAccessTokenAndRefreshToken(response, accessToken, refreshToken);
+
+        memberRepository.findByEmail(email)
+                .ifPresent(member -> {
+                    member.updateRefreshToken(refreshToken);
+                    memberRepository.saveAndFlush(member);
+                });
+
+        log.info("로그인에 성공하였습니다. 이메일 : {}", email);
+//        log.info("로그인에 성공하였습니다. AccessToken : {}", accessToken);
+        log.info("발급된 AccessToken 만료 기간 : {}", accessTokenExpiration);
+    }
+
+    private String extractUsername(Authentication authentication) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        return userDetails.getUsername();
+    }
+
+}
